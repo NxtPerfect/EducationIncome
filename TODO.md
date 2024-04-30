b94c840c11a83cce4a20ad446c3b61092ae77c95
NxtPerfect <alakaxan@protonmail.com>
2024-04-30 22:07:56 +0200

    Sorted results in bar chart
    improved performance in loading
    working on proper data average

--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
 - [x] Load csv
 - [x] Plot income vs education level
-- [ ] Sort results by amount
-- [ ] how to optimize plotting, 100k takes 50secs, there's 400k records
+- [x] Sort results by amount
+- [x] how to optimize plotting, 100k takes 50secs, there's 400k records
+- [ ] Shows average income by gender
+    get median of it
+    conclude?
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from multiprocessing import Process
 from time import perf_counter
+from collections import defaultdict
 
 DATA = "./data/incom vs education.csv"
 
@@ -24,19 +25,36 @@ def showPlot(data):
     plt.bar(data["Education level"], data["Both Sexes"])
     print("Finished loading.")
     plt.xticks(rotation=45)
-    plt.tight_layout()
     plt.show()
 
 def showPlotDebug(data):
+    plt.figure(figsize=(10,6))
     # Sort results by biggest
     data = data[data["Education level"] != "Total, all education levels"]
-    plt.title("Testing")
+    plt.title("Education level vs Income")
+    data = data.groupby("Education level", as_index=False)['Both Sexes'].mean().reset_index()
+    data = data.sort_values(by=["Both Sexes"], ascending=False)
+    # If i sum up the elements before the plot, it should be faster
+
     print("Loading data...")
     t1_start = perf_counter()
-    plt.bar(data["Education level"][:100_000], data["Both Sexes"][:100_000])
+    barlist = plt.bar(data["Education level"], data["Both Sexes"])
+
+    for i, item in enumerate(barlist):
+        if not i % 3:
+            item.set_color('b')
+            continue
+        if not i % 4:
+            item.set_color('g')
+            continue
+        if not i % 2:
+            item.set_color('r')
+            continue
     t1_stop = perf_counter()
     print(f'Finished loading in{(t1_stop-t1_start): .2f} seconds.')
     plt.xticks(rotation=45)
+    plt.grid(axis="y")
+    plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
     plt.tight_layout()
     plt.show()
 
