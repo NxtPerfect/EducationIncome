83487e48a6632c13e356a078b2ae2deae41dce90
NxtPerfect <alakaxan@protonmail.com>
2024-05-26 14:19:25 +0200

    Created data clusters
    visualized clusters

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sb
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.cluster import KMeans
 
 DATA = "./data/heart_failure_clinical_records.csv"
 
@@ -54,12 +56,15 @@ def showPlots(df: pd.DataFrame):
     # Corelation heatmap
     sb.heatmap(corr, annot=True, fmt=".2f", annot_kws={"size": 8},  cmap='coolwarm')
     st.pyplot(plt)
+    plt.close()
 
     # Pairplot
     relevant_columns = ['ejection_fraction', 'serum_sodium', 'serum_creatinine', 'time', 'creatinine_phosphokinase', 'DEATH_EVENT']
     data_relevant = df[relevant_columns]
+    plt.figure(figsize=(10, 6))
     sb.pairplot(data_relevant, hue="DEATH_EVENT")
     st.pyplot(plt)
+    plt.close()
 
     # Histograms of each column
     for column in df.columns:
@@ -68,6 +73,88 @@ def showPlots(df: pd.DataFrame):
             sb.histplot(df[column], kde=True, bins=30)
             plt.title(f'Distribution of {column}')
             st.pyplot(plt)
+            plt.close()
+    
+    # Clustering data
+    ## Elbow method
+    wcss = []
+    # data_scaled = StandardScaler().fit_transform(data_relevant.drop("DEATH_EVENT", axis=1))
+    data_scaled = StandardScaler().fit_transform(data_relevant)
+    for k in range(1, 21):
+        kmeanModel = KMeans(n_clusters=k)
+        kmeanModel.fit(data_scaled)
+        
+        wcss.append(kmeanModel.inertia_)
+
+    st.markdown("# Clustering")
+    st.markdown("## Metoda łokci")
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, 21), wcss, marker='o', linestyle='-') # 8 clusters are at the elbow point
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.title('Metoda łokci')
+    st.pyplot(plt)
+    plt.close()
+
+    st.markdown("## Scatters")
+    kmeanModel = KMeans(n_clusters=5)
+    cdf = df.copy(deep=True)
+    cdf['cluster'] = kmeanModel.fit_predict(data_scaled)
+    st.write("Cluster mean:")
+    st.table(cdf.groupby("cluster", sort=True).mean())
+    
+    """
+    above aberage serum_creatinine
+    below average serum_sodium
+    below average time
+    below average platelets
+    """
+    
+    plt.figure(figsize=(10, 6))
+    colors = mpl.colormaps['Paired'].resampled(5)
+    for cluster_id in range(5):
+        cluster_data = df[cdf['cluster'] == cluster_id]
+        plt.scatter(cluster_data['ejection_fraction'], cluster_data['serum_sodium'], color=colors(cluster_id/5), label=f'Cluster {cluster_id}', s=50)
+
+    # 'ejection_fraction', 'serum_sodium', 'serum_creatinine', 'time', 'creatinine_phosphokinase', 'DEATH_EVENT'
+
+    plt.xlabel(relevant_columns[0])
+    plt.ylabel(relevant_columns[1])
+    plt.title('Klasteryzacja pacjentów na podstawie frakcji wyrzutowej i sodu w soczewicy')
+    plt.legend(title="Clusters")
+    st.pyplot(plt)
+    plt.close()
+
+
+    plt.figure(figsize=(10, 6))
+    colors = mpl.colormaps['Paired'].resampled(5)
+    for cluster_id in range(5):
+        cluster_data = df[cdf['cluster'] == cluster_id]
+        plt.scatter(cluster_data['serum_sodium'], cluster_data['serum_creatinine'], color=colors(cluster_id/5), label=f'Cluster {cluster_id}', s=50)
+
+    # 'ejection_fraction', 'serum_sodium', 'serum_creatinine', 'time', 'creatinine_phosphokinase', 'DEATH_EVENT'
+
+    plt.xlabel(relevant_columns[1])
+    plt.ylabel(relevant_columns[2])
+    plt.title('Klasteryzacja pacjentów na podstawie sodu i kreatyniny w soczewicy')
+    plt.legend(title="Clusters")
+    st.pyplot(plt)
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    colors = mpl.colormaps['Paired'].resampled(5)
+    for cluster_id in range(5):
+        cluster_data = df[cdf['cluster'] == cluster_id]
+        plt.scatter(cluster_data['ejection_fraction'], cluster_data['creatinine_phosphokinase'], color=colors(cluster_id/5), label=f'Cluster {cluster_id}', s=50)
+
+    # 'ejection_fraction', 'serum_sodium', 'serum_creatinine', 'time', 'creatinine_phosphokinase', 'DEATH_EVENT'
+
+    plt.xlabel(relevant_columns[0])
+    plt.ylabel(relevant_columns[4])
+    plt.title('Klasteryzacja pacjentów na podstawie frakcji wyrzutu i fosfokinazy kreatyniny')
+    plt.legend(title="Clusters")
+    st.pyplot(plt)
+    plt.close()
     
 def create_model(df, model, name):
     x = df.drop("DEATH_EVENT", axis=1).drop("time", axis=1)
@@ -87,7 +174,7 @@ def create_model(df, model, name):
 
     # Predict
     y_pred = model.predict(x_test)
-    st.markdown(f'# {name}')
+    st.markdown(f'## {name}')
     showMetrics(y_test, y_pred)
 
 def showMetrics(y_test, y_pred):
@@ -103,6 +190,7 @@ def showMetrics(y_test, y_pred):
     ax.set_xlabel('Predicted')
     ax.set_ylabel('Actual')
     st.pyplot(fig)
+    plt.close()
 
     ## Classification report
     class_report = classification_report(y_test, y_pred, output_dict=True)
@@ -112,6 +200,8 @@ def showMetrics(y_test, y_pred):
 
 if __name__ == "__main__":
     df = loadCSV(DATA)
+
+    st.markdown("# Prediction Models")
 
     # Models
     logistic_reg = LogisticRegression()
